a82a66cfa65066cad459dad42852278bd5bd102a
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-05-06 13:45:57 -0500

    fixed errors

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -4,52 +4,67 @@
 # --> You may need to establish a connection to your localhost db here
 
 --  3. create the movies_db
-CREATE DATABASE movies_db;
+CREATE DATABASE IF NOT EXISTS movies_db;
 
 -- # 4. use the movies_db
 USE movies_db;
 
 -- # 5. drop the table(s) to which no other tables are dependent (none at first)
-DROP TABLE IF EXISTS movies_db;
+DROP TABLE IF EXISTS movie_genre;
+DROP TABLE IF EXISTS movie_actors;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS directors;
 
 -- # 6. map the json movie properties to movies table columns
 # --> start with just a movies table with all the columns found in the movie json properties
-CREATE TABLE IF NOT EXISTS movies (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    plot text,
-    posters text,
-    rating char(1),
+CREATE TABLE IF NOT EXISTS directors
+(
+    id   INT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    primary key (id)
+);
+DESCRIBE directors;
+
+CREATE TABLE IF NOT EXISTS movies
+(
+    id          INT unsigned NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    plot        text,
+    posters     text,
+    rating      char(1),
     director_id INT unsigned NOT NULL,
     primary key (id),
     foreign key (director_id) references directors (id)
 );
 DESCRIBE movies;
 
-CREATE TABLE IF NOT EXISTS directors (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
+CREATE TABLE IF NOT EXISTS genres (
+    id INT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(120),
     primary key (id)
 );
-DESCRIBE directors;
 
-CREATE TABLE IF NOT EXISTS movie_genre (
+CREATE TABLE IF NOT EXISTS movie_genre
+(
     movie_id INT unsigned NOT NULL,
     genre_id INT unsigned NOT NULL,
-    id INT not null,
-    foreign key (movie_id) references Movies (id),
-    foreign key (genre_id) references movie_genre (id)
+    foreign key (movie_id) references movies (id),
+    foreign key (genre_id) references genres (id)
 );
 DESCRIBE movie_genre;
 
-CREATE TABLE IF NOT EXISTS actors (
-    id INT unsigned NOT NULL,
-    name VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS actors
+(
+    id   INT unsigned NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    primary key (id)
 );
 DESCRIBE actors;
 
-CREATE TABLE IF NOT EXISTS movie_actors (
+CREATE TABLE IF NOT EXISTS movie_actors
+(
     movie_id INT unsigned NOT NULL,
     actor_id INT unsigned NOT NULL,
     foreign key (movie_id) references movies (id),
