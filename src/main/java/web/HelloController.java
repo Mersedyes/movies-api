7300bff2d321227e1d51336b3c124255f52893ce
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-05-09 05:35:48 -0500

    created new files for exercise individual walk through

---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,18 @@
+package web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HelloController {
+//    @GetMapping("/hello") //defines a method that should be invoked when a GET request is received for the specified URI
+//    @ResponseBody //tells Spring that whatever is returned from this method should be the body of our response
+//    public String sayHello(@PathVariable String name){
+//        return "Hello," + name + "!";
+
+    @GetMapping("/increment/{number}")
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
