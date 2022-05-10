b95e64c1da1b3234840aad344ea6e357691c01fd
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-05-09 20:35:24 -0500

    changed the moviescontroller.java

--- src/main/java/web/MoviesController.java
+++ src/main/java/web/MoviesController.java
@@ -1,4 +1,16 @@
 package web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/movies", headers = "Accept=application/json)"
 public class MoviesController {
+private List<Movie> example
+
 }
